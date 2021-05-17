3c3d1e6f4168356cef4b6981b947a6da9af0357e
RShifter12 <imanaev98@icloud.com>
2021-05-17 18:01:32 +0300

    1

---
+++ source/js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+var pageHeader = document.querySelector('.header');
+var headerToggle = document.querySelector('.header__toggle');
+
+pageHeader.classList.remove('page-header--nojs');
+
+headerToggle.addEventListener('click', function () {
+  if (pageHeader.classList.contains('header--closed')) {
+    pageHeader.classList.remove('header--closed');
+    pageHeader.classList.add('header--opened');
+  } else {
+    pageHeader.classList.add('header--closed');
+    pageHeader.classList.remove('header--opened');
+  }
+});
+
